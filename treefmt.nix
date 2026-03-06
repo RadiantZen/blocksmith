a87d03ede94423008faec202d33bcd635722cb60
RadiantZen <zen@radiantzen.net>
2026-03-06 22:27:54 +0200

    Add dev environment

---
+++ treefmt.nix
@@ -0,0 +1,14 @@
+let
+  sources = import ./npins;
+  pkgs = import sources.nixpkgs { };
+  treefmt = import sources.treefmt-nix;
+in
+treefmt.mkWrapper pkgs {
+  projectRootFile = ".git/config";
+  programs = {
+    nixfmt = {
+      enable = true;
+      strict = true;
+    };
+  };
+}
