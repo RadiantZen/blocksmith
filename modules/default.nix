7f9951c2aea48f864d27deef7c84bd0f336ce194
RadiantZen <zen@radiantzen.net>
2026-03-07 00:18:01 +0200

    Initial prototype

---
+++ modules/default.nix
@@ -0,0 +1,36 @@
+{
+  config,
+  lib,
+  ...
+}:
+
+let
+  rootZfsModule =
+    with lib.types;
+    submodule {
+      options = {
+        rootPool = lib.mkOptions { type = with lib.types; str; };
+        zfsDatasetsList = lib.mkOptions { type = with lib.types; listOf str; };
+      };
+    };
+in
+{
+  options.blocksmith = {
+    enable = lib.mkEnableOption "blocksmith";
+    rootZfs = lib.mkOptions { type = rootZfsModule; };
+  };
+
+  config = lib.mkIf config.blocksmith.enable {
+    fileSystems = (
+      lib.attrsets.mergeAttrsList (
+        builtins.map (dir: {
+          ${dir} = {
+            device = "${config.blocksmith.rootZfs.rootPool}${dir}";
+            fsType = "zfs";
+            options = [ "zfsutil" ];
+          };
+        }) config.blocksmith.rootZfs.zfsDatasetsList
+      )
+    );
+  };
+}
